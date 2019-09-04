5d45f07a0571bf2ad5d7ad020fd948d85ff69db5
plamen1982 <plamen.hristov@ymail.com>
2019-09-04 19:49:02 +0300

    firebase connected

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './firebase';
+
 
 export default function App() {
+  const { db }  = firebase;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+      const testCollectionRef = db.collection('test').onSnapshot(function(snapshotData) {
+        const data = snapshotData.docs.map(d => {
+          return { id: d.id, ...d.data() }
+        });
+        setData(data);
+      })
+
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {data.map(d => <Text key={d.id}>{d.name}</Text>)}
     </View>
   );
 }
---
+++ firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import 'firebase/firestore'
+var firebaseConfig = {
+    apiKey: "AIzaSyD6UUhwbjBO1o5Ny5epzxdLQItRZYW7PZk",
+    authDomain: "ishotdogasandwich-test.firebaseapp.com",
+    databaseURL: "https://ishotdogasandwich-test.firebaseio.com",
+    projectId: "ishotdogasandwich-test",
+    storageBucket: "ishotdogasandwich-test.appspot.com",
+    messagingSenderId: "182270411429",
+    appId: "1:182270411429:web:3ca86043ef392f69"
+  };
+  // Initialize Firebase
+export default firebaseConfig;
+
+  
